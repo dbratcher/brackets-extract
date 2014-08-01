2411a6c35363a4c0b015f3255fe767df505551b9
Drew Bratcher <drew@crowdspots.com>
2014-07-31 19:34:19 -0700

    Add file menu and context menu buttons

--- main.js
+++ main.js
@@ -8,4 +8,37 @@
 define(function (require, exports, module) {
   'use strict';
 
+  var CommandManager = brackets.getModule("command/CommandManager"),
+    Menus = brackets.getModule("command/Menus");
+
+  var COMMAND_ID = "me.drewbratcher.extract",
+    CONTEXTUAL_COMMAND_ID = "me.drewbratcher.extractContextual";
+
+  function extract() {
+    console.log('extracting');
+  }
+
+  CommandManager.register("Extract", COMMAND_ID, extract);
+
+  var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+
+  var windowsCommand = {
+    key: "Ctrl-Shift-E",
+    platform: "win"
+  };
+
+  var macCommand = {
+    key: "Cmd-Shift-E",
+    platform: "mac"
+  };
+
+  var command = [windowsCommand, macCommand];
+
+  menu.addMenuDivider();
+  menu.addMenuItem(COMMAND_ID, command);
+
+  CommandManager.register("Beautify", CONTEXTUAL_COMMAND_ID, extract);
+  var contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
+  contextMenu.addMenuItem(CONTEXTUAL_COMMAND_ID);
+
 });
