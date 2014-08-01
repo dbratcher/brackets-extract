80cc8caec381502a262383ab691798de47440033
Drew Bratcher <drew@crowdspots.com>
2014-07-31 20:22:48 -0700

    Created Dialog Box and Options

--- main.js
+++ main.js
@@ -9,13 +9,59 @@ define(function (require, exports, module) {
   'use strict';
 
   var CommandManager = brackets.getModule("command/CommandManager"),
-    Menus = brackets.getModule("command/Menus");
+    Menus = brackets.getModule("command/Menus"),
+    EditorManager = brackets.getModule("editor/EditorManager"),
+    Editor = brackets.getModule("editor/Editor").Editor,
+    Dialogs = brackets.getModule("widgets/Dialogs"),
+    DefaultDialogs = brackets.getModule("widgets/DefaultDialogs"),
+    DocumentManager = brackets.getModule("document/DocumentManager");
 
   var COMMAND_ID = "me.drewbratcher.extract",
     CONTEXTUAL_COMMAND_ID = "me.drewbratcher.extractContextual";
 
+  var dialog = require("text!dialog.html");
+
   function extract() {
-    console.log('extracting');
+    var indentChar, indentSize, formattedText;
+    var unformattedText, isSelection = false;
+    var useTabs = Editor.getUseTabChar();
+
+    if (useTabs) {
+      indentChar = '\t';
+      indentSize = 1;
+    } else {
+      indentChar = ' ';
+      indentSize = Editor.getSpaceUnits();
+    }
+
+    var editor = EditorManager.getCurrentFullEditor();
+    var selectedText = editor.getSelectedText();
+    var selection = editor.getSelection();
+
+    if (selectedText.length > 0) {
+      isSelection = true;
+      unformattedText = selectedText;
+    } else {
+      Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_ERR, "Extract Refactoring Error", "You must highligh some code for extraction.");
+      return;
+    }
+
+    var doc = DocumentManager.getCurrentDocument();
+
+    var language = doc.getLanguage();
+    var fileType = language._id;
+
+    switch (fileType) {
+
+    case 'javascript':
+      Dialogs.showModalDialogUsingTemplate(dialog).done(function (id) {
+        console.log(id);
+      });
+      break;
+    default:
+      Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_ERR, "Extract Refactoring Error", "Unsupported File: Must be Javascript.");
+      return;
+    }
   }
 
   CommandManager.register("Extract", COMMAND_ID, extract);
