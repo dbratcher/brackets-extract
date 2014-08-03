37b75246480cc2747a5b737a0c6f777b2cb9924f
Drew Bratcher <drew@crowdspots.com>
2014-08-03 11:48:46 -0700

    Added replace dialog options

--- nls/root/strings.js
+++ nls/root/strings.js
@@ -1,12 +1,16 @@
 define({
-    "COMMAND_NAME":                  "Extract",
-    "DIALOG_ERROR_NO_SELECTION":     "You must highlight some code for extraction.",
-    "DIALOG_ERROR_UNSUPPORTED_FILE": "Unsupported File: Must be Javascript.",
-    "DIALOG_ERROR_TITLE":            "Extract Refactoring Error",
-    "DIALOG_INPUT_PLACEHOLDER":      "extracted",
-    "DIALOG_LABEL_FUNCTION":         "Function",
-    "DIALOG_LABEL_VARIABLE":         "Variable",
-    "DIALOG_LABEL_EXTRACT_TO":       "Extract To",
-    "DIALOG_LABEL_NEW_NAME":         "New Name",
-    "DIALOG_TITLE":                  "Extract Refactoring"
+  "COMMAND_NAME": "Extract",
+  "DIALOG_ERROR_NO_SELECTION": "You must highlight some code for extraction.",
+  "DIALOG_ERROR_UNSUPPORTED_FILE": "Unsupported File: Must be Javascript.",
+  "DIALOG_ERROR_TITLE": "Extract Refactoring Error",
+  "DIALOG_INPUT_PLACEHOLDER": "extracted",
+  "DIALOG_LABEL_FUNCTION": "Function",
+  "DIALOG_LABEL_VARIABLE": "Variable",
+  "DIALOG_LABEL_EXTRACT_TO": "Extract To",
+  "DIALOG_LABEL_NEW_NAME": "New Name",
+  "DIALOG_LABEL_REPLACE": "Replace",
+  "DIALOG_LABEL_TEXT": "Selected Text",
+  "DIALOG_LABEL_ALL": "All Instances",
+  "DIALOG_LABEL_SELECT": "Select Instances",
+  "DIALOG_TITLE": "Extract Refactoring"
 });
